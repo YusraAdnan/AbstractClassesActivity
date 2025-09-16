3a618b6b4e4fee700ba589dd43636b2e15ca203a
YusraAdnan <101713869+YusraAdnan@users.noreply.github.com>
2025-09-16 07:55:39 +0200

    up

--- src/main/java/com/mycompany/abstractclasses_activity/Main.java
+++ src/main/java/com/mycompany/abstractclasses_activity/Main.java
@@ -16,12 +16,14 @@ public class Main
 
         System.out.print("Enter base salary: ");
         double baseSalary = scan.nextDouble();
+        scan.nextLine();
 
         /* We initialize the two types of objects that can be created. 
         We only populate the relevant objects only when we know which object needs to be populated */
         
-        FullTimeEmployee fl;
-        PartTimeEmployee pl;
+        FullTimeEmployee ft;
+        PartTimeEmployee pt;
+        
         
         Employee emp;
         
@@ -30,12 +32,10 @@ public class Main
            System.out.print("Enter bonus: ");
            double bonus = scan.nextDouble();
            scan.nextLine();
-           fl = new FullTimeEmployee(name, baseSalary, bonus);
-           fl.printDetails();
-           System.out.println("Total Pay: R" + fl.calculatePay());
-           
-          // emp = new FullTimeEmployee(name, baseSalary, bonus);
-           
+          // ft = new FullTimeEmployee(name, baseSalary, bonus);
+          // ft.printDetails();
+          // System.out.println("Total Pay: R" + ft.calculatePay());
+           emp = new FullTimeEmployee(name, baseSalary, bonus);
         } 
         else 
         {
@@ -45,12 +45,17 @@ public class Main
             System.out.print("Enter hourly rate: ");
             double rate = scan.nextDouble();
             scan.nextLine();
-            pl = new PartTimeEmployee(name, baseSalary, hours, rate);
-            pl.printDetails();
-           System.out.println("Total Pay: R" + pl.calculatePay());   
-           //emp = new PartTimeEmployee(name, baseSalary, hours, rate);
+           // pt = new PartTimeEmployee(name, baseSalary, hours, rate);
+           // pt.printDetails();
+           //System.out.println("Total Pay: R" + pt.calculatePay());   
+           emp = new PartTimeEmployee(name, baseSalary, hours, rate);
         }
-      //  emp.printDetails();
-       // System.out.println("Total Pay: R" + emp.calculatePay());    
+        /* this is dynamic method binding
+        - "emp" is a type of employee (Part or Full time)
+        - based on which if else block was executed the 'emp' is populated with that object
+        Therefore at runtime it decides to call methods either from the Full Time employee or Part time employee*/
+        emp.printDetails();
+        System.out.println("Total Pay: R" + emp.calculatePay());   
+
     }
 }
